651b590eebbb2949152872fd6ab397e6868cfdf6
Ambroz Bizjak <ambrop7@gmail.com>
2013-07-09 15:38:08 +0200

    implement AxisSharer and use it in aprinter.cpp

--- aprinter/printer/aprinter.cpp
+++ aprinter/printer/aprinter.cpp
@@ -46,6 +46,7 @@
 #include <aprinter/stepper/Steppers.h>
 #include <aprinter/stepper/AxisStepper.h>
 #include <aprinter/stepper/AxisSplitter.h>
+#include <aprinter/stepper/AxisSharer.h>
 
 #define CLOCK_TIMER_PRESCALER 2
 #define LED1_PIN AvrPin<AvrPortA, 4>
@@ -82,12 +83,6 @@ struct SerialRecvHandler;
 struct SerialSendHandler;
 struct DriverGetStepperHandler0;
 struct DriverGetStepperHandler1;
-struct DriverPullCmdHandler0;
-struct DriverPullCmdHandler1;
-struct DriverBufferFullHandler0;
-struct DriverBufferFullHandler1;
-struct DriverBufferEmptyHandler0;
-struct DriverBufferEmptyHandler1;
 
 typedef DebugObjectGroup<MyContext> MyDebugObjectGroup;
 typedef AvrClock<MyContext, CLOCK_TIMER_PRESCALER> MyClock;
@@ -100,8 +95,10 @@ typedef AvrSerial<MyContext, uint8_t, SERIAL_RX_BUFFER, SerialRecvHandler, uint8
 typedef Steppers<MyContext, STEPPERS> MySteppers;
 typedef SteppersStepper<MyContext, STEPPERS, 0> MySteppersStepper0;
 typedef SteppersStepper<MyContext, STEPPERS, 1> MySteppersStepper1;
-typedef AxisSplitter<MyContext, STEPPER_COMMAND_BUFFER_BITS, MySteppersStepper0, DriverGetStepperHandler0, AvrClockInterruptTimer_TC1_OCA, DriverPullCmdHandler0, DriverBufferFullHandler0, DriverBufferEmptyHandler0> MyAxisSplitter0;
-typedef AxisSplitter<MyContext, STEPPER_COMMAND_BUFFER_BITS, MySteppersStepper1, DriverGetStepperHandler1, AvrClockInterruptTimer_TC1_OCB, DriverPullCmdHandler1, DriverBufferFullHandler1, DriverBufferEmptyHandler1> MyAxisSplitter1;
+typedef AxisSharer<MyContext, STEPPER_COMMAND_BUFFER_BITS, MySteppersStepper0, DriverGetStepperHandler0, AvrClockInterruptTimer_TC1_OCA> MyAxisSharer0;
+typedef AxisSharer<MyContext, STEPPER_COMMAND_BUFFER_BITS, MySteppersStepper1, DriverGetStepperHandler1, AvrClockInterruptTimer_TC1_OCB> MyAxisSharer1;
+typedef AxisSharerUser<MyContext, STEPPER_COMMAND_BUFFER_BITS, MySteppersStepper0, DriverGetStepperHandler0, AvrClockInterruptTimer_TC1_OCA> MyAxisUser0;
+typedef AxisSharerUser<MyContext, STEPPER_COMMAND_BUFFER_BITS, MySteppersStepper1, DriverGetStepperHandler1, AvrClockInterruptTimer_TC1_OCB> MyAxisUser1;
 
 struct MyContext {
     typedef MyDebugObjectGroup DebugGroup;
@@ -133,8 +130,10 @@ static MySerial myserial;
 static bool blink_state;
 static MyClock::TimeType next_time;
 static MySteppers steppers;
-static MyAxisSplitter0 axis_splitter0;
-static MyAxisSplitter1 axis_splitter1;
+static MyAxisSharer0 axis_sharer0;
+static MyAxisSharer1 axis_sharer1;
+static MyAxisUser0 axis_user0;
+static MyAxisUser1 axis_user1;
 static int index0;
 static int index1;
 static int cnt0;
@@ -170,8 +169,8 @@ MyPinWatcherService * MyContext::pinWatcherService () const
 AMBRO_AVR_CLOCK_ISRS(myclock, MyContext())
 AMBRO_AVR_PIN_WATCHER_ISRS(mypinwatcherservice, MyContext())
 AMBRO_AVR_SERIAL_ISRS(myserial, MyContext())
-AMBRO_AVR_CLOCK_INTERRUPT_TIMER_TC1_OCA_ISRS(*axis_splitter0.getTimer(), MyContext())
-AMBRO_AVR_CLOCK_INTERRUPT_TIMER_TC1_OCB_ISRS(*axis_splitter1.getTimer(), MyContext())
+AMBRO_AVR_CLOCK_INTERRUPT_TIMER_TC1_OCA_ISRS(*axis_sharer0.getTimer(), MyContext())
+AMBRO_AVR_CLOCK_INTERRUPT_TIMER_TC1_OCB_ISRS(*axis_sharer1.getTimer(), MyContext())
 
 static void mytimer_handler (MyTimer *, MyContext c)
 {
@@ -185,13 +184,15 @@ static void pinwatcher_handler (MyPinWatcher *, MyContext c, bool state)
 {
     mypins.set<LED2_PIN>(c, !state);
     if (!prev_button && state) {
-        if (axis_splitter0.isRunning(c) || axis_splitter1.isRunning(c)) {
-            if (axis_splitter0.isRunning(c)) {
-                axis_splitter0.stop(c);
+        if (axis_user0.isActive(c) || axis_user1.isActive(c)) {
+            if (axis_user0.isActive(c)) {
+                axis_user0.getAxis(c)->stop(c);
+                axis_user0.deactivate(c);
                 steppers.getStepper<0>()->enable(c, false);
             }
-            if (axis_splitter1.isRunning(c)) {
-                axis_splitter1.stop(c);
+            if (axis_user1.isActive(c)) {
+                axis_user1.getAxis(c)->stop(c);
+                axis_user1.deactivate(c);
                 steppers.getStepper<1>()->enable(c, false);
             }
         } else {
@@ -202,8 +203,10 @@ static void pinwatcher_handler (MyPinWatcher *, MyContext c, bool state)
             full = 0;
             steppers.getStepper<0>()->enable(c, true);
             steppers.getStepper<1>()->enable(c, true);
-            axis_splitter0.start(c);
-            axis_splitter1.start(c);
+            axis_user0.activate(c);
+            axis_user1.activate(c);
+            axis_user0.getAxis(c)->start(c);
+            axis_user1.getAxis(c)->start(c);
         }
     }
     prev_button = state;
@@ -217,12 +220,12 @@ static void serial_send_handler (MySerial *, MyContext c)
 {
 }
 
-static MySteppersStepper0 * driver_get_stepper_handler0 (MyAxisSplitter0 *) 
+static MySteppersStepper0 * driver_get_stepper_handler0 (MyAxisSharer0 *) 
 {
     return steppers.getStepper<0>();
 }
 
-static MySteppersStepper1* driver_get_stepper_handler1 (MyAxisSplitter1 *)
+static MySteppersStepper1* driver_get_stepper_handler1 (MyAxisSharer1 *)
 {
     return steppers.getStepper<1>();
 }
@@ -232,17 +235,15 @@ static void full_common (MyContext c)
     full++;
     if (full == 2) {
         MyClock::TimeType start_time = myclock.getTime(c);
-        axis_splitter0.startStepping(c, start_time);
-        axis_splitter1.startStepping(c, start_time);
+        axis_user0.getAxis(c)->startStepping(c, start_time);
+        axis_user1.getAxis(c)->startStepping(c, start_time);
     }
 }
 
-static void driver_pull_cmd_handler0 (MyAxisSplitter0 *, MyContext c)
+static void pull_cmd_handler0 (MyAxisUser0 *, MyContext c)
 {
-    AMBRO_ASSERT(axis_splitter0.isRunning(c))
-    
     if (cnt0 == 7 * 6) {
-        if (!axis_splitter0.isStepping(c)) {
+        if (!axis_user0.getAxis(c)->isStepping(c)) {
             full_common(c);
         }
         return;
@@ -250,34 +251,32 @@ static void driver_pull_cmd_handler0 (MyAxisSplitter0 *, MyContext c)
     float t_scale = SPEED_T_SCALE * X_SCALE;
     switch (index0) {
         case 0:
-            axis_splitter0.commandDoneTest(c, true, X_SCALE * 20.0, 1.0 * t_scale, X_SCALE * 20.0);
+            axis_user0.getAxis(c)->commandDoneTest(c, true, X_SCALE * 20.0, 1.0 * t_scale, X_SCALE * 20.0);
             break;
         case 1:
-            axis_splitter0.commandDoneTest(c, true, X_SCALE * 120.0, 3.0 * t_scale, X_SCALE * 0.0);
+            axis_user0.getAxis(c)->commandDoneTest(c, true, X_SCALE * 120.0, 3.0 * t_scale, X_SCALE * 0.0);
             break;
         case 2:
-            axis_splitter0.commandDoneTest(c, true, X_SCALE * 20.0, 1.0 * t_scale, X_SCALE * -20.0);
+            axis_user0.getAxis(c)->commandDoneTest(c, true, X_SCALE * 20.0, 1.0 * t_scale, X_SCALE * -20.0);
             break;
         case 3:
-            axis_splitter0.commandDoneTest(c, false, X_SCALE * 20.0, 1.0 * t_scale, X_SCALE * 20.0);
+            axis_user0.getAxis(c)->commandDoneTest(c, false, X_SCALE * 20.0, 1.0 * t_scale, X_SCALE * 20.0);
             break;
         case 4:
-            axis_splitter0.commandDoneTest(c, false, X_SCALE * 120.0, 3.0 * t_scale, X_SCALE * 0.0);
+            axis_user0.getAxis(c)->commandDoneTest(c, false, X_SCALE * 120.0, 3.0 * t_scale, X_SCALE * 0.0);
             break;
         case 5:
-            axis_splitter0.commandDoneTest(c, false, X_SCALE * 20.0, 1.0 * t_scale, X_SCALE * -20.0);
+            axis_user0.getAxis(c)->commandDoneTest(c, false, X_SCALE * 20.0, 1.0 * t_scale, X_SCALE * -20.0);
             break;
     }
     index0 = (index0 + 1) % 6;
     cnt0++;
 }
 
-static void driver_pull_cmd_handler1 (MyAxisSplitter1 *, MyContext c)
+static void pull_cmd_handler1 (MyAxisUser1 *, MyContext c)
 {
-    AMBRO_ASSERT(axis_splitter1.isRunning(c))
-    
     if (cnt1 == 6 * 8) {
-        if (!axis_splitter1.isStepping(c)) {
+        if (!axis_user1.getAxis(c)->isStepping(c)) {
             full_common(c);
         }
         return;
@@ -285,53 +284,55 @@ static void driver_pull_cmd_handler1 (MyAxisSplitter1 *, MyContext c)
     float t_scale = SPEED_T_SCALE * Y_SCALE;
     switch (index1) {
         case 0:
-            axis_splitter1.commandDoneTest(c, true, Y_SCALE * 20.0, 1.0 * t_scale, Y_SCALE * 20.0);
+            axis_user1.getAxis(c)->commandDoneTest(c, true, Y_SCALE * 20.0, 1.0 * t_scale, Y_SCALE * 20.0);
             break;
         case 1:
-            axis_splitter1.commandDoneTest(c, true, Y_SCALE * 120.0, 3.0 * t_scale, Y_SCALE * 0.0);
+            axis_user1.getAxis(c)->commandDoneTest(c, true, Y_SCALE * 120.0, 3.0 * t_scale, Y_SCALE * 0.0);
             break;
         case 2:
-            axis_splitter1.commandDoneTest(c, true, Y_SCALE * 20.0, 1.0 * t_scale, Y_SCALE * -20.0);
+            axis_user1.getAxis(c)->commandDoneTest(c, true, Y_SCALE * 20.0, 1.0 * t_scale, Y_SCALE * -20.0);
             break;
         case 3:
-            axis_splitter1.commandDoneTest(c, true, Y_SCALE * 0.0, 2.0 * t_scale, Y_SCALE * 0.0);
+            axis_user1.getAxis(c)->commandDoneTest(c, true, Y_SCALE * 0.0, 2.0 * t_scale, Y_SCALE * 0.0);
             break;
         case 4:
-            axis_splitter1.commandDoneTest(c, false, Y_SCALE * 20.0, 1.0 * t_scale, Y_SCALE * 20.0);
+            axis_user1.getAxis(c)->commandDoneTest(c, false, Y_SCALE * 20.0, 1.0 * t_scale, Y_SCALE * 20.0);
             break;
         case 5:
-            axis_splitter1.commandDoneTest(c, false, Y_SCALE * 120.0, 3.0 * t_scale, Y_SCALE * 0.0);
+            axis_user1.getAxis(c)->commandDoneTest(c, false, Y_SCALE * 120.0, 3.0 * t_scale, Y_SCALE * 0.0);
             break;
         case 6:
-            axis_splitter1.commandDoneTest(c, false, Y_SCALE * 20.0, 1.0 * t_scale, Y_SCALE * -20.0);
+            axis_user1.getAxis(c)->commandDoneTest(c, false, Y_SCALE * 20.0, 1.0 * t_scale, Y_SCALE * -20.0);
             break;
         case 7:
-            axis_splitter1.commandDoneTest(c, false, Y_SCALE * 0.0, 2.0 * t_scale, Y_SCALE * 0.0);
+            axis_user1.getAxis(c)->commandDoneTest(c, false, Y_SCALE * 0.0, 2.0 * t_scale, Y_SCALE * 0.0);
             break;
     }
     index1 = (index1 + 1) % 8;
     cnt1++;
 }
 
-static void driver_buffer_full_handler0 (MyAxisSplitter0 *, MyContext c)
+static void buffer_full_handler0 (MyAxisUser0 *, MyContext c)
 {
     full_common(c);
 }
 
-static void driver_buffer_full_handler1 (MyAxisSplitter1 *, MyContext c)
+static void buffer_full_handler1 (MyAxisUser1 *, MyContext c)
 {
     full_common(c);
 }
 
-static void driver_buffer_empty_handler0 (MyAxisSplitter0 *, MyContext c)
+static void buffer_empty_handler0 (MyAxisUser0 *, MyContext c)
 {
-    axis_splitter0.stop(c);
+    axis_user0.getAxis(c)->stop(c);
+    axis_user0.deactivate(c);
     steppers.getStepper<0>()->enable(c, false);
 }
 
-static void driver_buffer_empty_handler1 (MyAxisSplitter1 *, MyContext c)
+static void buffer_empty_handler1 (MyAxisUser1 *, MyContext c)
 {
-    axis_splitter1.stop(c);
+    axis_user1.getAxis(c)->stop(c);
+    axis_user1.deactivate(c);
     steppers.getStepper<1>()->enable(c, false);
 }
 
@@ -340,12 +341,6 @@ struct SerialRecvHandler : public AMBRO_WFUNC(serial_recv_handler) {};
 struct SerialSendHandler : public AMBRO_WFUNC(serial_send_handler) {};
 struct DriverGetStepperHandler0 : public AMBRO_WFUNC(driver_get_stepper_handler0) {};
 struct DriverGetStepperHandler1 : public AMBRO_WFUNC(driver_get_stepper_handler1) {};
-struct DriverPullCmdHandler0 : public AMBRO_WFUNC(driver_pull_cmd_handler0) {};
-struct DriverPullCmdHandler1 : public AMBRO_WFUNC(driver_pull_cmd_handler1) {};
-struct DriverBufferFullHandler0 : public AMBRO_WFUNC(driver_buffer_full_handler0) {};
-struct DriverBufferFullHandler1 : public AMBRO_WFUNC(driver_buffer_full_handler1) {};
-struct DriverBufferEmptyHandler0 : public AMBRO_WFUNC(driver_buffer_empty_handler0) {};
-struct DriverBufferEmptyHandler1 : public AMBRO_WFUNC(driver_buffer_empty_handler1) {};
 
 FILE uart_output;
 
@@ -382,8 +377,10 @@ int main ()
     setup_uart_stdio();
     printf("HELLO\n");
     steppers.init(c);
-    axis_splitter0.init(c);
-    axis_splitter1.init(c);
+    axis_sharer0.init(c);
+    axis_sharer1.init(c);
+    axis_user0.init(c, &axis_sharer0, pull_cmd_handler0, buffer_full_handler0, buffer_empty_handler0);
+    axis_user1.init(c, &axis_sharer1, pull_cmd_handler1, buffer_full_handler1, buffer_empty_handler1);
     
     mypins.setOutput<LED1_PIN>(c);
     mypins.setOutput<LED2_PIN>(c);
---
+++ aprinter/stepper/AxisSharer.h
@@ -0,0 +1,195 @@
+/*
+ * Copyright (c) 2013 Ambroz Bizjak
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef AMBROLIB_AXIS_SHARER_H
+#define AMBROLIB_AXIS_SHARER_H
+
+#include <stddef.h>
+
+#include <aprinter/meta/WrapCallback.h>
+#include <aprinter/base/OffsetCallback.h>
+#include <aprinter/base/DebugObject.h>
+#include <aprinter/base/Assert.h>
+#include <aprinter/stepper/AxisSplitter.h>
+
+#include <aprinter/BeginNamespace.h>
+
+template <typename Context, int StepperBufferBits, typename Stepper, typename GetStepperHandler, template<typename, typename> class StepperTimer>
+class AxisSharerUser;
+
+template <typename Context, int StepperBufferBits, typename Stepper, typename GetStepperHandler, template<typename, typename> class StepperTimer>
+class AxisSharer
+: private DebugObject<Context, void>
+{
+private:
+    struct AxisGetStepperHandler;
+    struct AxisPullCmdHandler;
+    struct AxisBufferFullHandler;
+    struct AxisBufferEmptyHandler;
+    
+public:
+    using Axis = AxisSplitter<Context, StepperBufferBits, Stepper, AxisGetStepperHandler, StepperTimer, AxisPullCmdHandler, AxisBufferFullHandler, AxisBufferEmptyHandler>;
+    using User = AxisSharerUser<Context, StepperBufferBits, Stepper, GetStepperHandler, StepperTimer>;
+    
+    void init (Context c)
+    {
+        m_axis.init(c);
+        m_user = NULL;
+        
+        this->debugInit(c);
+    }
+    
+    void deinit (Context c)
+    {
+        this->debugDeinit(c);
+        AMBRO_ASSERT(!m_user)
+        
+        m_axis.deinit(c);
+    }
+    
+    typename Axis::TimerInstance * getTimer ()
+    {
+        return m_axis.getTimer();
+    }
+    
+private:
+    friend User;
+    
+    Stepper * axisGetStepperHandler ()
+    {
+        return GetStepperHandler::call(this);
+    }
+    
+    void axisPullCmdHandler (Context c)
+    {
+        this->debugAccess(c);
+        AMBRO_ASSERT(m_user)
+        
+        return m_user->m_pull_cmd_handler(m_user, c);
+    }
+    
+    void axisBufferFullHandler (Context c)
+    {
+        this->debugAccess(c);
+        AMBRO_ASSERT(m_user)
+        
+        return m_user->m_buffer_full_handler(m_user, c);
+    }
+    
+    void axisBufferEmptyHandler (Context c)
+    {
+        this->debugAccess(c);
+        AMBRO_ASSERT(m_user)
+        
+        return m_user->m_buffer_empty_handler(m_user, c);
+    }
+    
+    Axis m_axis;
+    User *m_user;
+    
+    struct AxisGetStepperHandler : public AMBRO_WCALLBACK_TD(&AxisSharer::axisGetStepperHandler, &AxisSharer::m_axis) {};
+    struct AxisPullCmdHandler : public AMBRO_WCALLBACK_TD(&AxisSharer::axisPullCmdHandler, &AxisSharer::m_axis) {};
+    struct AxisBufferFullHandler : public AMBRO_WCALLBACK_TD(&AxisSharer::axisBufferFullHandler, &AxisSharer::m_axis) {};
+    struct AxisBufferEmptyHandler : public AMBRO_WCALLBACK_TD(&AxisSharer::axisBufferEmptyHandler, &AxisSharer::m_axis) {};
+};
+
+template <typename Context, int StepperBufferBits, typename Stepper, typename GetStepperHandler, template<typename, typename> class StepperTimer>
+class AxisSharerUser
+: private DebugObject<Context, void>
+{
+public:
+    using Sharer = AxisSharer<Context, StepperBufferBits, Stepper, GetStepperHandler, StepperTimer>;
+    using Axis = typename Sharer::Axis;
+    
+    using PullCmdHandler = void (*) (AxisSharerUser *, Context);
+    using BufferFullHandler = void (*) (AxisSharerUser *, Context);
+    using BufferEmptyHandler = void (*) (AxisSharerUser *, Context);
+    
+    void init (Context c, Sharer *sharer, PullCmdHandler pull_cmd_handler, BufferFullHandler buffer_full_handler, BufferEmptyHandler buffer_empty_handler)
+    {
+        m_sharer = sharer;
+        m_pull_cmd_handler = pull_cmd_handler;
+        m_buffer_full_handler = buffer_full_handler;
+        m_buffer_empty_handler = buffer_empty_handler;
+        m_active = false;
+        
+        this->debugInit(c);
+    }
+    
+    void deinit (Context c)
+    {
+        this->debugDeinit(c);
+        AMBRO_ASSERT(!m_active)
+    }
+    
+    void activate (Context c)
+    {
+        this->debugAccess(c);
+        AMBRO_ASSERT(!m_active)
+        AMBRO_ASSERT(!m_sharer->m_user)
+        AMBRO_ASSERT(!m_sharer->m_axis.isRunning(c))
+        
+        m_active = true;
+        m_sharer->m_user = this;
+    }
+    
+    void deactivate (Context c)
+    {
+        this->debugAccess(c);
+        AMBRO_ASSERT(m_active)
+        AMBRO_ASSERT(!m_sharer->m_axis.isRunning(c))
+        AMBRO_ASSERT(m_sharer->m_user == this)
+        
+        m_active = false;
+        m_sharer->m_user = NULL;
+    }
+    
+    Axis * getAxis (Context c)
+    {
+        this->debugAccess(c);
+        AMBRO_ASSERT(m_active)
+        
+        return &m_sharer->m_axis;
+    }
+    
+    bool isActive (Context c)
+    {
+        this->debugAccess(c);
+        
+        return m_active;
+    }
+    
+private:
+    friend Sharer;
+    
+    Sharer *m_sharer;
+    PullCmdHandler m_pull_cmd_handler;
+    BufferFullHandler m_buffer_full_handler;
+    BufferEmptyHandler m_buffer_empty_handler;
+    bool m_active;
+};
+
+#include <aprinter/EndNamespace.h>
+
+#endif
