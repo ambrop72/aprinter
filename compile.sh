c91955e3bffe1a274c00ee2154b8e5b5bc30b3dc
Ambroz Bizjak <ambrop7@gmail.com>
2013-07-15 19:27:54 +0200

    compile.sh: compile as c++1y

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@ CROSS=${CROSS:-avr-}
 CXX=${CXX:-${CROSS}g++}
 MCU=${MCU:-atmega1284p}
 F_CPU=${F_CPU:-20000000}
-CXXFLAGS="-std=c++11 -mmcu=$MCU -DF_CPU=$F_CPU -DNDEBUG -O2 -fwhole-program -g \
+CXXFLAGS="-std=c++1y -mmcu=$MCU -DF_CPU=$F_CPU -DNDEBUG -O2 -fwhole-program -g \
 -ffunction-sections -fdata-sections -Wl,--gc-sections \
 -D__STDC_LIMIT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_CONSTANT_MACROS \
 -DAMBROLIB_AVR -I. $CXXFLAGS"
