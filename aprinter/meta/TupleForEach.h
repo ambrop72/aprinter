588aa78da51024b669ad0f7324e46804b796e77a
Ambroz Bizjak <ambrop7@gmail.com>
2013-07-15 18:38:46 +0200

    TupleForEach: implement accumulating result foreach, and add helper functions

--- aprinter/meta/TupleForEach.h
+++ aprinter/meta/TupleForEach.h
@@ -60,6 +60,12 @@ struct TupleForEach<Tuple<ConsTypeList<Head, Tail>>> {
         }
         return TupleForEach<TailTupleType>::call_forward_interruptible(tuple->getTail(), func, args...);
     }
+    
+    template <typename AccRes, typename Func, typename... Args>
+    static auto call_forward_accres (TupleType *tuple, AccRes acc_res, Func func, Args... args) -> decltype(TupleForEach<TailTupleType>::call_forward_accres(tuple->getTail(), func(&tuple->elem, acc_res, args...), func, args...))
+    {
+        return TupleForEach<TailTupleType>::call_forward_accres(tuple->getTail(), func(&tuple->elem, acc_res, args...), func, args...);
+    }
 };
 
 template <>
@@ -81,6 +87,39 @@ struct TupleForEach<Tuple<EmptyTypeList>> {
     {
         return true;
     }
+    
+    template <typename AccRes, typename Func, typename... Args>
+    static auto call_forward_accres (TupleType *tuple, AccRes acc_res, Func func, Args... args) -> decltype(acc_res)
+    {
+        return acc_res;
+    }
+};
+
+template <typename TupleType, typename Func, typename... Args>
+void TupleForEachForward (TupleType *tuple, Func func, Args... args)
+{
+    return TupleForEach<TupleType>::call_forward(tuple, func, args...);
+}
+
+template <typename TupleType, typename Func, typename... Args>
+void TupleForEachReverse (TupleType *tuple, Func func, Args... args)
+{
+    return TupleForEach<TupleType>::call_reverse(tuple, func, args...);
+}
+
+template <typename TupleType, typename InitialAccRes, typename Func, typename... Args>
+auto TupleForEachForwardAccRes (TupleType *tuple, InitialAccRes initial_acc_res, Func func, Args... args) -> decltype(TupleForEach<TupleType>::call_forward_accres(tuple, initial_acc_res, func, args...))
+{
+    return TupleForEach<TupleType>::call_forward_accres(tuple, initial_acc_res, func, args...);
+}
+
+#define AMBRO_DECLARE_TUPLE_FOREACH_HELPER(helper_name, func_name) \
+struct helper_name { \
+    template <typename TupleForEachElemPtrType, typename... TupleForEachArgs> \
+    auto operator() (TupleForEachElemPtrType tuple_for_each_elem, TupleForEachArgs... tuple_for_each_args) -> decltype(tuple_for_each_elem->func_name(tuple_for_each_args...)) \
+    { \
+        return tuple_for_each_elem->func_name(tuple_for_each_args...); \
+    } \
 };
 
 #include <aprinter/EndNamespace.h>
