20c7665c61284b0465f25d2d1cfd694ce6935051
Ambroz Bizjak <ambrop7@gmail.com>
2013-09-19 15:24:41 +0200

    Add RAMPS1.3/1.4 port (untested).

---
+++ aprinter/printer/arduino_mega_pins.h
@@ -0,0 +1,96 @@
+#ifndef AMBROLIB_ARDUINO_MEGA_PINS_H
+#define AMBROLIB_ARDUINO_MEGA_PINS_H
+
+#include <aprinter/BeginNamespace.h>
+
+using MegaPin0 = AvrPin<AvrPortE, 0>;
+using MegaPin1 = AvrPin<AvrPortE, 1>;
+using MegaPin2 = AvrPin<AvrPortE, 4>;
+using MegaPin3 = AvrPin<AvrPortE, 5>;
+using MegaPin4 = AvrPin<AvrPortG, 5>;
+using MegaPin5 = AvrPin<AvrPortE, 3>;
+using MegaPin6 = AvrPin<AvrPortH, 3>;
+using MegaPin7 = AvrPin<AvrPortH, 4>;
+using MegaPin8 = AvrPin<AvrPortH, 5>;
+using MegaPin9 = AvrPin<AvrPortH, 6>;
+using MegaPin10 = AvrPin<AvrPortB, 4>;
+using MegaPin11 = AvrPin<AvrPortB, 5>;
+using MegaPin12 = AvrPin<AvrPortB, 6>;
+using MegaPin13 = AvrPin<AvrPortB, 7>;
+using MegaPin14 = AvrPin<AvrPortJ, 1>;
+using MegaPin15 = AvrPin<AvrPortJ, 0>;
+using MegaPin16 = AvrPin<AvrPortH, 1>;
+using MegaPin17 = AvrPin<AvrPortH, 0>;
+using MegaPin18 = AvrPin<AvrPortD, 3>;
+using MegaPin19 = AvrPin<AvrPortD, 2>;
+using MegaPin20 = AvrPin<AvrPortD, 1>;
+using MegaPin21 = AvrPin<AvrPortD, 0>;
+using MegaPin22 = AvrPin<AvrPortA, 0>;
+using MegaPin23 = AvrPin<AvrPortA, 1>;
+using MegaPin24 = AvrPin<AvrPortA, 2>;
+using MegaPin25 = AvrPin<AvrPortA, 3>;
+using MegaPin26 = AvrPin<AvrPortA, 4>;
+using MegaPin27 = AvrPin<AvrPortA, 5>;
+using MegaPin28 = AvrPin<AvrPortA, 6>;
+using MegaPin29 = AvrPin<AvrPortA, 7>;
+using MegaPin30 = AvrPin<AvrPortC, 7>;
+using MegaPin31 = AvrPin<AvrPortC, 6>;
+using MegaPin32 = AvrPin<AvrPortC, 5>;
+using MegaPin33 = AvrPin<AvrPortC, 4>;
+using MegaPin34 = AvrPin<AvrPortC, 3>;
+using MegaPin35 = AvrPin<AvrPortC, 2>;
+using MegaPin36 = AvrPin<AvrPortC, 1>;
+using MegaPin37 = AvrPin<AvrPortC, 0>;
+using MegaPin38 = AvrPin<AvrPortD, 2>;
+using MegaPin39 = AvrPin<AvrPortG, 2>;
+using MegaPin40 = AvrPin<AvrPortG, 1>;
+using MegaPin41 = AvrPin<AvrPortG, 0>;
+using MegaPin42 = AvrPin<AvrPortL, 7>;
+using MegaPin43 = AvrPin<AvrPortL, 6>;
+using MegaPin44 = AvrPin<AvrPortL, 5>;
+using MegaPin45 = AvrPin<AvrPortL, 4>;
+using MegaPin46 = AvrPin<AvrPortL, 3>;
+using MegaPin47 = AvrPin<AvrPortL, 2>;
+using MegaPin48 = AvrPin<AvrPortL, 1>;
+using MegaPin49 = AvrPin<AvrPortL, 0>;
+using MegaPin50 = AvrPin<AvrPortB, 3>;
+using MegaPin51 = AvrPin<AvrPortB, 2>;
+using MegaPin52 = AvrPin<AvrPortB, 1>;
+using MegaPin53 = AvrPin<AvrPortB, 0>;
+using MegaPin54 = AvrPin<AvrPortF, 0>;
+using MegaPin55 = AvrPin<AvrPortF, 1>;
+using MegaPin56 = AvrPin<AvrPortF, 2>;
+using MegaPin57 = AvrPin<AvrPortF, 3>;
+using MegaPin58 = AvrPin<AvrPortF, 4>;
+using MegaPin59 = AvrPin<AvrPortF, 5>;
+using MegaPin60 = AvrPin<AvrPortF, 6>;
+using MegaPin61 = AvrPin<AvrPortF, 7>;
+using MegaPin62 = AvrPin<AvrPortK, 0>;
+using MegaPin63 = AvrPin<AvrPortK, 1>;
+using MegaPin64 = AvrPin<AvrPortK, 2>;
+using MegaPin65 = AvrPin<AvrPortK, 3>;
+using MegaPin66 = AvrPin<AvrPortK, 4>;
+using MegaPin67 = AvrPin<AvrPortK, 5>;
+using MegaPin68 = AvrPin<AvrPortK, 6>;
+using MegaPin69 = AvrPin<AvrPortK, 7>;
+
+using MegaPinA0 = MegaPin54;
+using MegaPinA1 = MegaPin55;
+using MegaPinA2 = MegaPin56;
+using MegaPinA3 = MegaPin57;
+using MegaPinA4 = MegaPin58;
+using MegaPinA5 = MegaPin59;
+using MegaPinA6 = MegaPin60;
+using MegaPinA7 = MegaPin61;
+using MegaPinA8 = MegaPin62;
+using MegaPinA9 = MegaPin63;
+using MegaPinA10 = MegaPin64;
+using MegaPinA11 = MegaPin65;
+using MegaPinA12 = MegaPin66;
+using MegaPinA13 = MegaPin67;
+using MegaPinA14 = MegaPin68;
+using MegaPinA15 = MegaPin69;
+
+#include <aprinter/EndNamespace.h>
+
+#endif
