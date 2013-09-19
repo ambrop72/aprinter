092d34b9f8bd7fc5cd864bdeae886e0538987f1d
Ambroz Bizjak <ambrop7@gmail.com>
2013-09-19 15:51:47 +0200

    compile.sh: remove ability to pass configuration via env vars, adjust defaults to my current setup

--- compile.sh
+++ compile.sh
@@ -3,14 +3,17 @@
 set -e
 set -x
 
-CROSS=${CROSS:-avr-}
-CXX=${CXX:-${CROSS}g++}
-MCU=${MCU:-atmega1284p}
-F_CPU=${F_CPU:-20000000}
+CROSS=avr-
+CXX=${CROSS}g++
+
+MCU=atmega1284p
+F_CPU=16000000
+MAIN=aprinter/printer/aprinter-melzi.cpp
+
 CXXFLAGS="-std=c++11 -mmcu=$MCU -DF_CPU=$F_CPU -DNDEBUG -O2 -fwhole-program -g \
 -ffunction-sections -fdata-sections -Wl,--gc-sections \
 -D__STDC_LIMIT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_CONSTANT_MACROS \
 -DAMBROLIB_AVR -I. $CXXFLAGS"
 
-${CXX} $CXXFLAGS aprinter/printer/aprinter.cpp -o aprinter.elf -Wl,-u,vfprintf -lprintf_flt
+${CXX} $CXXFLAGS ${MAIN} -o aprinter.elf -Wl,-u,vfprintf -lprintf_flt
 ${CROSS}objcopy -j .text -j .data -O ihex aprinter.elf aprinter.hex
