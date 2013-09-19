6246d4285e1e1fde71abc06aac906bddb6be383c
Ambroz Bizjak <ambrop7@gmail.com>
2013-09-19 12:43:41 +0200

    AvrAdc: support atmega2560 and other MCUs in this class (untested)

--- aprinter/system/AvrAdc.h
+++ aprinter/system/AvrAdc.h
@@ -35,16 +35,22 @@
 #include <aprinter/meta/TypeListIndex.h>
 #include <aprinter/meta/IsEqualFunc.h>
 #include <aprinter/meta/TupleGet.h>
-#include <aprinter/meta/TypesAreEqual.h>
 #include <aprinter/meta/TypeListLength.h>
 #include <aprinter/meta/TupleForEach.h>
 #include <aprinter/meta/WrapMember.h>
+#include <aprinter/meta/MakeTypeList.h>
 #include <aprinter/base/DebugObject.h>
 #include <aprinter/base/Lock.h>
 #include <aprinter/system/AvrPins.h>
 #include <aprinter/system/AvrLock.h>
 
 #include <aprinter/BeginNamespace.h>
+
+template <typename TPin1, typename TPin2, int TGain>
+struct AvrAdcDifferentialInput {};
+
+struct AvrAdcVbgPin {};
+struct AvrAdcGndPin {};
 
 template <typename Context, typename PinsList, int AdcRefSel, int AdcPrescaler>
 class AvrAdc
@@ -54,6 +60,145 @@ private:
     static const int NumPins = TypeListLength<PinsList>::value;
     AMBRO_DECLARE_TUPLE_FOREACH_HELPER(Foreach_make_pin_mask, make_pin_mask)
     AMBRO_DECLARE_TUPLE_FOREACH_HELPER(Foreach_handle_isr, handle_isr)
+    
+    using MaskType = uint16_t;
+    
+    template <typename AdcPin>
+    struct AdcPinMask {
+        static const MaskType value = 0;
+    };
+    
+    template <typename Pin1, typename Pin2, int Gain>
+    struct AdcPinMask<AvrAdcDifferentialInput<Pin1, Pin2, Gain>> {
+        static const MaskType value = (AdcPinMask<Pin1>::value | AdcPinMask<Pin2>::value);
+    };
+    
+#if defined(__AVR_ATmega164A__) || defined(__AVR_ATmega164PA__) || defined(__AVR_ATmega324A__) || \
+    defined(__AVR_ATmega324PA__) || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644PA__) || \
+    defined(__AVR_ATmega128__) || defined(__AVR_ATmega1284P__)
+    
+    using AdcList = MakeTypeList<
+        AvrPin<AvrPortA, 0>,
+        AvrPin<AvrPortA, 1>,
+        AvrPin<AvrPortA, 2>,
+        AvrPin<AvrPortA, 3>,
+        AvrPin<AvrPortA, 4>,
+        AvrPin<AvrPortA, 5>,
+        AvrPin<AvrPortA, 6>,
+        AvrPin<AvrPortA, 7>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 0>, AvrPin<AvrPortA, 0>, 10>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 1>, AvrPin<AvrPortA, 0>, 10>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 0>, AvrPin<AvrPortA, 0>, 200>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 1>, AvrPin<AvrPortA, 0>, 200>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 2>, AvrPin<AvrPortA, 2>, 10>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 3>, AvrPin<AvrPortA, 2>, 10>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 2>, AvrPin<AvrPortA, 2>, 200>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 3>, AvrPin<AvrPortA, 2>, 200>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 0>, AvrPin<AvrPortA, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 1>, AvrPin<AvrPortA, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 2>, AvrPin<AvrPortA, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 3>, AvrPin<AvrPortA, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 4>, AvrPin<AvrPortA, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 5>, AvrPin<AvrPortA, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 6>, AvrPin<AvrPortA, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 7>, AvrPin<AvrPortA, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 0>, AvrPin<AvrPortA, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 1>, AvrPin<AvrPortA, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 2>, AvrPin<AvrPortA, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 3>, AvrPin<AvrPortA, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 4>, AvrPin<AvrPortA, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortA, 5>, AvrPin<AvrPortA, 2>, 1>,
+        AvrAdcVbgPin,
+        AvrAdcGndPin
+    >;
+    
+    template <int PortPinIndex>
+    struct AdcPinMask<AvrPin<AvrPortA, PortPinIndex>> {
+        static const MaskType value = ((MaskType)1 << PortPinIndex);
+    };
+    
+#elif defined(__AVR_ATmega640__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega1281__) || \
+    defined(__AVR_ATmega2560__) || defined(__AVR_ATmega2561__)
+    
+    using AdcList = MakeTypeList<
+        AvrPin<AvrPortF, 0>,
+        AvrPin<AvrPortF, 1>,
+        AvrPin<AvrPortF, 2>,
+        AvrPin<AvrPortF, 3>,
+        AvrPin<AvrPortF, 4>,
+        AvrPin<AvrPortF, 5>,
+        AvrPin<AvrPortF, 6>,
+        AvrPin<AvrPortF, 7>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 0>, AvrPin<AvrPortF, 0>, 10>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 1>, AvrPin<AvrPortF, 0>, 10>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 0>, AvrPin<AvrPortF, 0>, 200>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 1>, AvrPin<AvrPortF, 0>, 200>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 2>, AvrPin<AvrPortF, 2>, 10>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 3>, AvrPin<AvrPortF, 2>, 10>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 2>, AvrPin<AvrPortF, 2>, 200>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 3>, AvrPin<AvrPortF, 2>, 200>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 0>, AvrPin<AvrPortF, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 1>, AvrPin<AvrPortF, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 2>, AvrPin<AvrPortF, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 3>, AvrPin<AvrPortF, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 4>, AvrPin<AvrPortF, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 5>, AvrPin<AvrPortF, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 6>, AvrPin<AvrPortF, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 7>, AvrPin<AvrPortF, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 0>, AvrPin<AvrPortF, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 1>, AvrPin<AvrPortF, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 2>, AvrPin<AvrPortF, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 3>, AvrPin<AvrPortF, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 4>, AvrPin<AvrPortF, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortF, 5>, AvrPin<AvrPortF, 2>, 1>,
+        AvrAdcVbgPin,
+        AvrAdcGndPin,
+        AvrPin<AvrPortK, 0>,
+        AvrPin<AvrPortK, 1>,
+        AvrPin<AvrPortK, 2>,
+        AvrPin<AvrPortK, 3>,
+        AvrPin<AvrPortK, 4>,
+        AvrPin<AvrPortK, 5>,
+        AvrPin<AvrPortK, 6>,
+        AvrPin<AvrPortK, 7>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 0>, AvrPin<AvrPortK, 0>, 10>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 1>, AvrPin<AvrPortK, 0>, 10>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 0>, AvrPin<AvrPortK, 0>, 200>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 1>, AvrPin<AvrPortK, 0>, 200>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 2>, AvrPin<AvrPortK, 2>, 10>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 3>, AvrPin<AvrPortK, 2>, 10>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 2>, AvrPin<AvrPortK, 2>, 200>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 3>, AvrPin<AvrPortK, 2>, 200>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 0>, AvrPin<AvrPortK, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 1>, AvrPin<AvrPortK, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 2>, AvrPin<AvrPortK, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 3>, AvrPin<AvrPortK, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 4>, AvrPin<AvrPortK, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 5>, AvrPin<AvrPortK, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 6>, AvrPin<AvrPortK, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 7>, AvrPin<AvrPortK, 1>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 0>, AvrPin<AvrPortK, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 1>, AvrPin<AvrPortK, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 2>, AvrPin<AvrPortK, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 3>, AvrPin<AvrPortK, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 4>, AvrPin<AvrPortK, 2>, 1>,
+        AvrAdcDifferentialInput<AvrPin<AvrPortK, 5>, AvrPin<AvrPortK, 2>, 1>
+        
+    >;
+    
+    template <int PortPinIndex>
+    struct AdcPinMask<AvrPin<AvrPortF, PortPinIndex>> {
+        static const MaskType value = ((MaskType)1 << PortPinIndex);
+    };
+    
+    template <int PortPinIndex>
+    struct AdcPinMask<AvrPin<AvrPortK, PortPinIndex>> {
+        static const MaskType value = ((MaskType)1 << (8 + PortPinIndex));
+    };
+    
+#else
+#error Your device is not supported by AvrAdc
+#endif
     
 public:
     void init (Context c)
@@ -96,11 +241,18 @@ private:
             o->m_current_pin = 0;
             o->m_finished = false;
             
-            uint8_t mask = 0;
-            TupleForEachForward(&o->m_pins, Foreach_make_pin_mask(), &mask);
+            MaskType mask = TupleForEachForwardAccRes(&o->m_pins, 0, Foreach_make_pin_mask());
             DIDR0 = mask;
-            ADMUX = (AdcRefSel << REFS0) | AdcPin<0>::Pin::port_pin;
+#ifdef DIDR2
+            DIDR2 = mask >> 8;
+#endif
+#ifdef MUX5
+            ADMUX = (AdcRefSel << REFS0) | (AdcPin<0>::AdcIndex & 0x1F);
+            ADCSRB = (AdcPin<0>::AdcIndex >> 5) << MUX5;
+#else
+            ADMUX = (AdcRefSel << REFS0) | AdcPin<0>::AdcIndex;
             ADCSRB = 0;
+#endif
             ADCSRA = (1 << ADEN) | (1 << ADSC) | (1 << ADIE) | (AdcPrescaler << ADPS0);
             
             while (!*(volatile bool *)&o->m_finished);
@@ -111,6 +263,9 @@ private:
             ADCSRA = 0;
             ADCSRB = 0;
             ADMUX = 0;
+#ifdef DIDR2
+            DIDR2 = 0;
+#endif
             DIDR0 = 0;
         }
     };
@@ -124,8 +279,7 @@ private:
     template <int PinIndex>
     struct AdcPin {
         using Pin = TypeListGet<PinsList, PinIndex>;
-        static_assert(TypesAreEqual<typename Pin::Port, AvrPortA>::value, "");
-        static_assert(Pin::port_pin < 8, "");
+        static const int AdcIndex = TypeListIndex<AdcList, IsEqualFunc<Pin>>::value;
         static const int NextPinIndex = (PinIndex + 1) % NumPins;
         
         AvrAdc * parent ()
@@ -133,9 +287,9 @@ private:
             return AMBRO_WMEMB_TD(&AvrAdc::m_pins)::container(TupleGetTuple<PinIndex, PinsTuple>(this));
         }
         
-        void make_pin_mask (uint8_t *out)
+        MaskType make_pin_mask (MaskType accum)
         {
-            *out |= (1 << Pin::port_pin);
+            return (accum | AdcPinMask<Pin>::value);
         }
         
         bool handle_isr (AvrInterruptContext<Context> c)
@@ -144,7 +298,12 @@ private:
                 return true;
             }
             m_value = ADC;
-            ADMUX = (AdcRefSel << REFS0) | AdcPin<NextPinIndex>::Pin::port_pin;
+#ifdef MUX5
+            ADMUX = (AdcRefSel << REFS0) | (AdcPin<NextPinIndex>::AdcIndex & 0x1F);
+            ADCSRB = (AdcPin<NextPinIndex>::AdcIndex >> 5) << MUX5;
+#else
+            ADMUX = (AdcRefSel << REFS0) | AdcPin<NextPinIndex>::AdcIndex;
+#endif
             ADCSRA = (1 << ADEN) | (1 << ADSC) | (1 << ADIE) | (AdcPrescaler << ADPS0);
             parent()->m_current_pin = NextPinIndex;
             if (PinIndex == NumPins - 1) {
