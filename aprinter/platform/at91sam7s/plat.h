4b2e28fb3dc99fe3e746da3cacdea79bca14b913
Ambroz Bizjak <ambrop7@gmail.com>
2013-11-23 00:36:39 +0100

    Remove incomplete At91Sam7s port.

--- aprinter/platform/at91sam7s/plat.h
+++
@@ -1,5 +0,0 @@
-#ifdef AT91SAM7S512
-#include "AT91SAM7S512.h"
-#else
-#error Unknown platform
-#endif
--- flash-at91sam7s.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-
-openocd -f aprinter/platform/at91sam7s/openocd-flash.cfg -c "myflash aprinter.bin" -c "reset run" -c exit
