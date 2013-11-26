316baeb1b94987a449dde5248ba994de027a3c5f
Ambroz Bizjak <ambrop7@gmail.com>
2013-11-26 22:08:11 +0100

    Remove existing compile.sh and flash.sh, add board-specific scripts instead.

---
+++ compile-ramps13.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -x
+
+CROSS=avr-
+CXX=${CROSS}g++
+
+MCU=atmega2560
+F_CPU=16000000
+MAIN=aprinter/printer/aprinter-ramps13.cpp
+
+CXXFLAGS="-std=c++11 -mmcu=$MCU -DF_CPU=$F_CPU -DNDEBUG -O2 -fwhole-program -g \
+-ffunction-sections -fdata-sections -Wl,--gc-sections \
+-D__STDC_LIMIT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_CONSTANT_MACROS \
+-DAMBROLIB_AVR -I. $CXXFLAGS"
+
+${CXX} $CXXFLAGS ${MAIN} -o aprinter.elf -Wl,-u,vfprintf -lprintf_flt
+${CROSS}objcopy -j .text -j .data -O ihex aprinter.elf aprinter.hex
--- flash.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-avrdude -p atmega1284p -c stk500v2 -P /dev/ttyACM0 -U flash:w:aprinter.hex
