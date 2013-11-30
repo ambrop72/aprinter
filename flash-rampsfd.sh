19ba8ca14c9072c2cd7444dd1e034c26c3911e65
Ambroz Bizjak <ambrop7@gmail.com>
2013-11-30 17:56:00 +0100

    flash-rampsfd.sh: put serial port to 1200 baud first to force bootloader mode

--- flash-rampsfd.sh
+++ flash-rampsfd.sh
@@ -3,4 +3,9 @@
 set -e
 set -x
 
-/home/ambro/arduino-1.5.4/hardware/tools/bossac -p ttyACM0 -U false -i -e -w -v -b aprinter.bin -R
+PORT=ttyACM0
+BOSSAC=/home/ambro/arduino-1.5.4/hardware/tools/bossac
+
+stty -F /dev/${PORT} 1200
+sleep 0.5
+"$BOSSAC" -p ${PORT} -U false -i -e -w -v -b aprinter.bin -R
