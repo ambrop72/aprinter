a78bbb599ed33ed700a63368eb6d93669ff5359c
Ambroz Bizjak <ambrop7@gmail.com>
2014-03-02 15:37:40 +0100

    compile scripts: Quote the compiler command where not already. So we can compile even if compiler path has spaces.

--- compile-ramps13.sh
+++ compile-ramps13.sh
@@ -17,5 +17,5 @@ CXXFLAGS="-std=c++11 -mmcu=$MCU -DF_CPU=$F_CPU -DNDEBUG -O2 -fwhole-program \
 -D__STDC_LIMIT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_CONSTANT_MACROS \
 -DAMBROLIB_AVR -I. $CXXFLAGS"
 
-${CXX} $CXXFLAGS ${MAIN} -o out/aprinter.elf -Wl,-u,vfprintf -lprintf_flt
-${CROSS}objcopy -j .text -j .data -O ihex out/aprinter.elf out/aprinter.hex
+"${CXX}" $CXXFLAGS ${MAIN} -o out/aprinter.elf -Wl,-u,vfprintf -lprintf_flt
+"${CROSS}objcopy" -j .text -j .data -O ihex out/aprinter.elf out/aprinter.hex
