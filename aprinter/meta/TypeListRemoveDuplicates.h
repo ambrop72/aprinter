f5d6d7d350910bbe82c53320c94dd9f28d2ac041
Ambroz Bizjak <ambrop7@gmail.com>
2014-06-15 09:12:57 +0200

    Add TypeListRemoveDuplicates.h.

---
+++ aprinter/meta/TypeListRemoveDuplicates.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2014 Ambroz Bizjak
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef AMBROLIB_TYPE_LIST_REMOVE_DUPLICATES_H
+#define AMBROLIB_TYPE_LIST_REMOVE_DUPLICATES_H
+
+#include <aprinter/meta/TypeList.h>
+#include <aprinter/meta/TypeListIndex.h>
+#include <aprinter/meta/If.h>
+#include <aprinter/meta/IsEqualFunc.h>
+#include <aprinter/meta/TypeListReverse.h>
+
+#include <aprinter/BeginNamespace.h>
+
+template <typename Current, typename Remain>
+struct TypeListRemoveDuplicatesHelper;
+
+template <typename Current, typename Head, typename Tail>
+struct TypeListRemoveDuplicatesHelper<Current, ConsTypeList<Head, Tail>> {
+    using Type = typename TypeListRemoveDuplicatesHelper<
+        If<
+            (TypeListIndex<Current, IsEqualFunc<Head>>::Value == -1),
+            ConsTypeList<Head, Current>,
+            Current
+        >,
+        Tail
+    >::Type;
+};
+
+template <typename Current>
+struct TypeListRemoveDuplicatesHelper<Current, EmptyTypeList> {
+    using Type = Current;
+};
+
+template <typename List>
+using TypeListRemoveDuplicates = TypeListReverse<typename TypeListRemoveDuplicatesHelper<EmptyTypeList, List>::Type>;
+
+#include <aprinter/EndNamespace.h>
+
+#endif
