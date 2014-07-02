2c702c21cf13f8f79acd63b6c5e7725c879a968e
Ambroz Bizjak <ambrop7@gmail.com>
2014-07-03 01:21:38 +0200

    Add ExprFixedPoint, udpate expr_test.

--- tests/expr_test.cpp
+++ tests/expr_test.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <inttypes.h>
 
 #include <aprinter/meta/Expr.h>
+#include <aprinter/meta/ExprFixedPoint.h>
 #include <aprinter/meta/TypesAreEqual.h>
 #include <aprinter/meta/WrapDouble.h>
 
@@ -9,20 +11,21 @@ using namespace APrinter;
 // Functions returning variable values.
 struct VarFuncC { static bool call () { return 1; } };
 struct VarFuncD { static char call () { return 0; } };
+struct VarFuncG { static double call () { return 123.7; } };
 
 // Define floating point constants.
-using FpValE = AMBRO_WRAP_DOUBLE(1.2);
-using FpValF = AMBRO_WRAP_DOUBLE(3.5);
 
 // Define constant expressions.
 using ConstantA = SimpleConstantExpr<int, 3>;
 using ConstantB = SimpleConstantExpr<int, 2>;
-using ConstantE = ConstantExpr<double, FpValE>;
-using ConstantF = ConstantExpr<double, FpValF>;
+using ConstantE = APRINTER_FP_CONST_EXPR(1.2);
+using ConstantF = APRINTER_FP_CONST_EXPR(3.5);
+using ConstantH = APRINTER_FP_CONST_EXPR(123.7);
 
 // Define variable expressions.
 using VariableC = VariableExpr<bool, VarFuncC>;
 using VariableD = VariableExpr<char, VarFuncD>;
+using VariableG = VariableExpr<double, VarFuncG>;
 
 int main ()
 {
@@ -53,13 +56,21 @@ int main ()
     static_assert(TypesAreEqual<Test1::Type, double>::Value, "");
     static_assert(!Test1::IsConstexpr, "");
     
+    using MyFixedType = FixedPoint<16, false, 0>;
+    using Fixed1 = decltype(ExprFixedPointImport<MyFixedType>(ConstantH()));
+    using Fixed2 = decltype(ExprFixedPointImport<MyFixedType>(VariableG()));
+    
     // Try some evaluations.
-    // If an expression is constexpr, its eval function must be constexpr.
-    constexpr int aplusb = APlusB::eval();
-    constexpr double eplusf = EplusF::eval();
+    // If an expression is constexpr, its must have a constexpr value() function.
+    constexpr int aplusb = APlusB::value();
+    constexpr double eplusf = EplusF::value();
     double test1 = Test1::eval();
+    constexpr MyFixedType fixed1 = Fixed1::value();
+    MyFixedType fixed2 = Fixed2::eval();
     
     printf("%d\n", aplusb);
     printf("%f\n", eplusf);
     printf("%f\n", test1);
+    printf("%" PRIu16 "\n", fixed1.m_bits.m_int);
+    printf("%" PRIu16 "\n", fixed2.m_bits.m_int);
 }
