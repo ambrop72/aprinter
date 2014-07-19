49a491a9a6bcca6aeed44e42adbbae25d7a74ca7
Ambroz Bizjak <ambrop7@gmail.com>
2014-07-19 20:27:19 +0200

    Add StaticArray and test.

---
+++ tests/static_array_test.cpp
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <stdio.h>
+
+#include <aprinter/meta/StaticArray.h>
+
+using namespace APrinter;
+
+template <int Index>
+struct ElemValue {
+    static constexpr double value () { return Index + 0.5; }
+};
+
+using TheArray = StaticArray<double, 10, ElemValue>;
+
+int main ()
+{
+    for (size_t i = 0; i < TheArray::Length; i++) {
+        printf("%f\n", TheArray::data[i]);
+    }
+}
