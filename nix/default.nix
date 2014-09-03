f6adc06fb9da4fce299e60ec4725d4bfd09f37a2
Ambroz Bizjak <ambrop7@gmail.com>
2014-09-03 21:44:46 +0200

    Add teensy3-corexy-laser Nix expression.

--- nix/default.nix
+++ nix/default.nix
@@ -14,24 +14,25 @@ rec {
     
     aprinterSymlinksFunc = aprinterBuilds: pkgs.callPackage ./symlinks.nix { inherit aprinterBuilds; };
     
-    aprinterTestFunc = boardName: aprinterFunc {
+    aprinterTestFunc = boardName: { sourceName ? boardName }: aprinterFunc {
         inherit boardName;
-        buildName = "test-${boardName}";
-        mainText = builtins.readFile ( ../main + "/aprinter-${boardName}.cpp" );
+        buildName = "test-${sourceName}";
+        mainText = builtins.readFile ( ../main + "/aprinter-${sourceName}.cpp" );
     };
     
-    aprinterTestMelzi = aprinterTestFunc "melzi";
-    aprinterTestRamps13 = aprinterTestFunc "ramps13";
-    aprinterTestMegatronics3 = aprinterTestFunc "megatronics3";
-    aprinterTestRampsfd = aprinterTestFunc "rampsfd";
-    aprinterTestRadds = aprinterTestFunc "radds";
-    aprinterTest4pi = aprinterTestFunc "4pi";
-    aprinterTestMlab = aprinterTestFunc "mlab";
-    aprinterTestTeensy3 = aprinterTestFunc "teensy3";
+    aprinterTestMelzi = aprinterTestFunc "melzi" {};
+    aprinterTestRamps13 = aprinterTestFunc "ramps13" {};
+    aprinterTestMegatronics3 = aprinterTestFunc "megatronics3" {};
+    aprinterTestRampsfd = aprinterTestFunc "rampsfd" {};
+    aprinterTestRadds = aprinterTestFunc "radds" {};
+    aprinterTest4pi = aprinterTestFunc "4pi" {};
+    aprinterTestMlab = aprinterTestFunc "mlab" {};
+    aprinterTestTeensy3 = aprinterTestFunc "teensy3" {};
+    aprinterTestCoreXyLaser = aprinterTestFunc "teensy3" { sourceName = "teensy3-corexy-laser"; };
     
     aprinterTestAll = aprinterSymlinksFunc [
         aprinterTestMelzi aprinterTestRamps13 aprinterTestMegatronics3
         aprinterTestRampsfd aprinterTestRadds aprinterTest4pi
-        aprinterTestMlab aprinterTestTeensy3
+        aprinterTestMlab aprinterTestTeensy3 aprinterTestCoreXyLaser
     ];
 }
