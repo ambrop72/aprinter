4eb8fa04122fa8b2e365446f3cb9a73a9957536e
Ambroz Bizjak <ambrop7@gmail.com>
2015-01-24 12:40:11 +0100

    Nix build: Update NCD version.

--- nix/ncd.nix
+++ nix/ncd.nix
@@ -9,13 +9,11 @@ stdenv.mkDerivation {
   
   src = fetchgit {
     url = https://github.com/ambrop72/badvpn;
-    rev = "cce6adc03994f46c10f58a91a313211272a41de3";
-    sha256 = "1x3l4m8yl86bm0p3lfw8glfc7l16f8pxjnhsawfa8bavyrmfbai0";
+    rev = "f282fbf4d2bcf4747fa4c14cddd83c4d6f4f135f";
+    sha256 = "038dsk89d3yf3pk07w7gkmw588i6178q6x4dv2amdjfgv8mclq37";
   };
 
   preConfigure = ''
-    find . -name '*.sh' -exec sed -e 's@#!/bin/sh@${stdenv.shell}@' -i '{}' ';'
-    find . -name '*.sh' -exec sed -e 's@#!/bin/bash@${bash}/bin/bash@' -i '{}' ';'
     cmakeFlagsArray=("-DCMAKE_BUILD_TYPE=" "-DCMAKE_C_FLAGS=${compileFlags}" -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_NCD=1);
   '';
 }
