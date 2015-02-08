7ef5ac8dc6f4a8f7154446413f494cfd2d6a051d
Ambroz Bizjak <ambrop7@gmail.com>
2015-02-08 23:26:29 +0100

    meta/TypeListGet: Optimize using an index. This results in a small compile time improvement (1.3%).

--- aprinter/meta/TypeListGet.h
+++ aprinter/meta/TypeListGet.h
@@ -26,24 +26,51 @@
 #define AMBROLIB_TYPE_LIST_GET_H
 
 #include <aprinter/meta/TypeList.h>
+#include <aprinter/meta/TypeListLength.h>
 
 #include <aprinter/BeginNamespace.h>
 
+namespace Private {
+    template <typename List, int Length>
+    struct BuildListIndex {
+        static int const LeftCount = Length / 2;
+        using Left = BuildListIndex<List, LeftCount>;
+        using Right = BuildListIndex<typename Left::Remain, Length - LeftCount>;
+        using Remain = typename Right::Remain;
+        
+        template <int Index, bool OnLeft>
+        struct GetHelper {
+            using Result = typename Left::template Get<Index>;
+        };
+        
+        template <int Index>
+        struct GetHelper<Index, false> {
+            using Result = typename Right::template Get<Index - LeftCount>;
+        };
+        
+        template <int Index>
+        using Get = typename GetHelper<Index, (Index < LeftCount)>::Result;
+    };
+    
+    template <typename Head, typename Tail>
+    struct BuildListIndex<ConsTypeList<Head, Tail>, 1> {
+        using Remain = Tail;
+        
+        template <int Index>
+        using Get = Head;
+    };
+    
+    template <typename List, int Index>
+    struct ListIndexGetHelper {
+        static int const Length = TypeListLength<List>::Value;
+        static_assert(Index >= 0, "Element index is too small.");
+        static_assert(Index < Length, "Element index is too large.");
+        using Result = typename BuildListIndex<List, Length>::template Get<Index>;
+    };
+}
+
 template <typename List, int Index>
-struct TypeListGetHelper;
-
-template <typename Head, typename Tail>
-struct TypeListGetHelper<ConsTypeList<Head, Tail>, 0> {
-    typedef Head Type;
-};
-
-template <typename Head, typename Tail, int Index>
-struct TypeListGetHelper<ConsTypeList<Head, Tail>, Index> {
-    typedef typename TypeListGetHelper<Tail, (Index - 1)>::Type Type;
-};
-
-template <typename List, int Index>
-using TypeListGet = typename TypeListGetHelper<List, Index>::Type;
+using TypeListGet = typename Private::ListIndexGetHelper<List, Index>::Result;
 
 template <typename List>
 struct TypeListGetFunc {
