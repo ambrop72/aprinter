37a8adb6d86aad5965891c94e76c817850ea5449
Ambroz Bizjak <ambrop7@gmail.com>
2015-04-12 13:03:18 +0200

    LinearPlanner: Memory usage and speed optimization.

--- aprinter/printer/LinearPlanner.h
+++ aprinter/printer/LinearPlanner.h
@@ -39,7 +39,6 @@ struct LinearPlanner {
         FpType max_v;
         FpType max_start_v;
         FpType a_x_rec;
-        FpType two_max_v_minus_a_x;
     };
 
     class SegmentState {
@@ -65,7 +64,6 @@ struct LinearPlanner {
         segment->max_start_v = FloatMin(prev_max_v, FloatMin(max_start_v, max_v));
         segment->a_x = a_x;
         segment->a_x_rec = 1.0f / a_x;
-        segment->two_max_v_minus_a_x = 2 * max_v - a_x;
     }
     
     AMBRO_OPTIMIZE_SPEED
@@ -92,21 +90,22 @@ struct LinearPlanner {
         
         FpType end_v = s->end_v;
         
-        if (end_v > start_v + segment->a_x) {
-            end_v = start_v + segment->a_x;
+        FpType start_v_plus_a_x = start_v + segment->a_x;
+        if (end_v > start_v_plus_a_x) {
+            end_v = start_v_plus_a_x;
             result->const_start = 1.0f;
             result->const_end = 0.0f;
             result->const_v = end_v;
         } else {
-            if (start_v + end_v > segment->two_max_v_minus_a_x) {
+            FpType half_start_v_plus_a_x_plus_end_v = FloatLdexp(start_v_plus_a_x + end_v, -1);
+            if (half_start_v_plus_a_x_plus_end_v > segment->max_v) {
                 result->const_start = (segment->max_v - start_v) * segment->a_x_rec;
                 result->const_end = (segment->max_v - end_v) * segment->a_x_rec;
                 result->const_v = segment->max_v;
             } else {
-                FpType q = end_v + segment->a_x;
-                result->const_start = (q - start_v) * segment->a_x_rec / 2;
+                result->const_start = (half_start_v_plus_a_x_plus_end_v - start_v) * segment->a_x_rec;
                 result->const_end = 1.0f - result->const_start;
-                result->const_v = (q + start_v) / 2;
+                result->const_v = half_start_v_plus_a_x_plus_end_v;
             }
         }
         
