140db623b405346462e93f6ee8fe3e9f847a4e11
Ambroz Bizjak <ambrop7@gmail.com>
2016-03-22 23:43:36 +0100

    lwip: fixed compiler warnings where passing variable instance to ip_addr_isany
    Upstream commit: bc3089916869161c7681f3a2a1e0382a03ef4b18

--- lwip/src/core/udp.c
+++ lwip/src/core/udp.c
@@ -568,7 +568,7 @@ udp_sendto(struct udp_pcb *pcb, struct pbuf *p,
          administratively selected interface for multicast by default.
          However, this can be overridden by setting an interface address
          in pcb->multicast_ip that is used for routing. */
-      if (!ip_addr_isany(&pcb->multicast_ip) &&
+      if (!ip_addr_isany_val(pcb->multicast_ip) &&
           !ip4_addr_cmp(ip_2_ip4(&pcb->multicast_ip), IP4_ADDR_BROADCAST)) {
         dst_ip_route = & pcb->multicast_ip;
       }
