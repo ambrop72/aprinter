4d7de726a5d3b11daa7bd5ba2cc9b70f990871c7
Ambroz Bizjak <ambrop7@gmail.com>
2016-03-23 01:02:54 +0100

    lwip: Drop unnecessary NULL test for h->next->next in memp_sanity
    Upstream commit: 9c6708513a015930e2a9e3e4639156b6cda65e13

--- lwip/src/core/memp.c
+++ lwip/src/core/memp.c
@@ -185,7 +185,7 @@ memp_sanity(void)
     t = memp_tab[i];
     if (t != NULL) {
       for (h = t->next; (t != NULL) && (h != NULL); t = t->next,
-        h = (((h->next != NULL) && (h->next->next != NULL)) ? h->next->next : NULL)) {
+        h = ((h->next != NULL) ? h->next->next : NULL)) {
         if (t == h) {
           return 0;
         }
