5a331f14260d657e19b2a1ab1b46972589e61c3c
Ambroz Bizjak <ambrop7@gmail.com>
2016-05-21 22:56:32 +0200

    Add teensy-core patch: Use gcc memory barriers in IRQ enable/disable

--- nix/teensy_cores.nix
+++ nix/teensy_cores.nix
@@ -22,9 +22,24 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-{ fetchgit }:
-fetchgit {
-    url = https://github.com/PaulStoffregen/cores;
-    rev = "e0fdda191818db356ce111440a19c090a76bb221";
-    sha256 = "11v8bqxalba28cyk4hks44hjj6nnk4xdsyc1zvh55gccdlvmf785";
+{ stdenv, fetchgit }:
+stdenv.mkDerivation {
+    name = "teensy-cores";
+    
+    src = fetchgit {
+        url = https://github.com/PaulStoffregen/cores;
+        rev = "e0fdda191818db356ce111440a19c090a76bb221";
+        sha256 = "11v8bqxalba28cyk4hks44hjj6nnk4xdsyc1zvh55gccdlvmf785";
+    };
+    
+    patches = [ ../patches/teensy-cores-irq.patch ];
+    
+    installPhase = ''
+        mkdir -p $out
+        cp -r . $out/
+    '';
+    
+    dontStrip = true;
+    dontPatchELF = true;
+    dontPatchShebangs = true;
 }
