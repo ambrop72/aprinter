73b3c1bc0e23864e81b6d1f3fb55675b83324b0c
Ambroz Bizjak <ambrop7@gmail.com>
2016-06-10 17:58:35 +0200

    lwip: Fix several doxygen errors all over the code
    Upstream commit: 90a656ed78756316f6339422fbd47703f968b017

--- lwip/src/core/pbuf.c
+++ lwip/src/core/pbuf.c
@@ -272,7 +272,7 @@ pbuf_alloc_pool(pbuf_layer layer, u16_t length, u16_t min_first_len)
 #if LWIP_SUPPORT_CUSTOM_PBUF
 /** Initialize a custom pbuf (already allocated).
  *
- * @param layer flag to define header size
+ * @param l flag to define header size
  * @param length size of the pbuf's payload
  * @param type type of the pbuf (only used to treat the pbuf accordingly, as
  *        this function allocates no memory)
@@ -934,6 +934,7 @@ pbuf_take(struct pbuf *buf, const void *dataptr, u16_t len)
  * @param buf pbuf to fill with data
  * @param dataptr application supplied data buffer
  * @param len length of the application supplied data buffer
+ * @param offset offset in pbuf where to copy dataptr to
  *
  * @return ERR_OK if successful, ERR_MEM if the pbuf is not big enough
  */
