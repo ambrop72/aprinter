272563897b177b79c96ab4efd6b1b79fa9d4a86a
Ambroz Bizjak <ambrop7@gmail.com>
2017-01-21 15:00:43 +0100

    Buf: Make processBytes eagerly consume buffers, documentation.

--- aipstack/misc/Buf.h
+++ aipstack/misc/Buf.h
@@ -34,22 +34,81 @@
 
 #include <aipstack/BeginNamespace.h>
 
+/**
+ * Node in a chain of memory buffers.
+ * 
+ * It contains the pointer to and length of a buffer,
+ * and a pointer to the next node, if any.
+ */
 struct IpBufNode {
+    /**
+     * Pointer to the buffer data.
+     */
     char *ptr;
+    
+    /**
+     * Length of the buffer.
+     */
     size_t len;
+    
+    /**
+     * Pointer to the next buffer node (or NULL).
+     */
     IpBufNode const *next;
 };
 
+/**
+ * Reference to a possibly discontiguous range of memory in
+ * a chain of memory buffers.
+ * 
+ * It contains the pointer to the first buffer node,
+ * the byte offset within that first buffer, and the total
+ * length of the memory range.
+ * 
+ * Except where noted otherwise, all functions in IpBufRef
+ * require the reference to be *valid*. This means that
+ * node is not NULL, the offset points to a valid location
+ * within this first buffer (pointing to the end is permitted),
+ * and there is at least tot_len remaining data in the first
+ * buffer and subsequent buffers together.
+ * 
+ * Operations with these memory ranges never modify the
+ * buffer nodes (IpBufNode). Only the IpBufRef objects are
+ * changed or created to refer to different ranges of a
+ * buffer chain.
+ */
 struct IpBufRef {
+    /**
+     * Pointer to the first buffer node.
+     */
     IpBufNode const *node;
+    
+    /**
+     * Byte offset in the first buffer.
+     */
     size_t offset;
+    
+    /**
+     * The total length of the data range.
+     */
     size_t tot_len;
     
+    /**
+     * Returns tot_len.
+     * 
+     * A valid reference is not needed, this simply returns tot_len
+     * without any requirements.
+     */
     inline size_t getTotalLength () const
     {
         return tot_len;
     }
     
+    /**
+     * Returns the pointer to the first chunk of the memory range.
+     * 
+     * This returns node->ptr + offset.
+     */
     inline char * getChunkPtr () const
     {
         AMBRO_ASSERT(node != nullptr)
@@ -58,6 +117,11 @@ struct IpBufRef {
         return node->ptr + offset;
     }
     
+    /**
+     * Returns the length of the first chunk of the memory range.
+     * 
+     * This returns min(tot_len, node->len - offset).
+     */
     inline size_t getChunkLength () const
     {
         AMBRO_ASSERT(node != nullptr)
@@ -66,6 +130,14 @@ struct IpBufRef {
         return APrinter::MinValue(tot_len, (size_t)(node->len - offset));
     }
     
+    /**
+     * Move to the next buffer in the memory range.
+     * 
+     * This decrements tot_len by getChunkLength(), sets node to
+     * node->next and sets offset to 0. After that it returns
+     * whether there is any more data in the (now modified) memory
+     * range, that is (tot_len > 0).
+     */
     inline bool nextChunk ()
     {
         AMBRO_ASSERT(node != nullptr)
@@ -81,6 +153,17 @@ struct IpBufRef {
         return more;
     }
     
+    /**
+     * Attempts to extend the memory range backward in the
+     * first buffer.
+     * 
+     * If amount is greater than offset, returns false since
+     * insufficient memory is available in the first buffer.
+     * Otherwise, sets *new_ref to the memory region extended
+     * to the left by amount and returns true. The *new_ref
+     * will have the same node, offset decremented by amount
+     * and tot_len incremented by amount.
+     */
     inline bool revealHeader (size_t amount, IpBufRef *new_ref) const
     {
         if (amount > offset) {
@@ -95,11 +178,25 @@ struct IpBufRef {
         return true;
     }
     
+    /**
+     * Checks if there is at least amount bytes available
+     * in the first chunk of the memory range.
+     * 
+     * It returns (getChunkLength() >= amount).
+     */
     inline bool hasHeader (size_t amount) const
     {
         return getChunkLength() >= amount;
     }
     
+    /**
+     * Returns a memory range with without an initial portion
+     * of this memory range.
+     * 
+     * The amount must be lesser than or equal to getChunkLength().
+     * It returns a reference with the same node, offset
+     * incremented by amount and tot_len decremented by amount.
+     */
     inline IpBufRef hideHeader (size_t amount) const
     {
         AMBRO_ASSERT(node != nullptr)
@@ -114,6 +211,14 @@ struct IpBufRef {
         };
     }
     
+    /**
+     * Returns an IpBufNode corresponding for the first buffer
+     * of the memory range with the offset applied.
+     * 
+     * It returns an IpBufNode with ptr equal to node->ptr
+     * + offset, len equal to node->len - offset and next
+     * equal to node->next.
+     */
     inline IpBufNode toNode () const
     {
         AMBRO_ASSERT(node != nullptr)
@@ -126,7 +231,32 @@ struct IpBufRef {
         };
     }
     
-    inline IpBufRef subHeaderToContinuedBy (size_t header_len, IpBufNode const *cont, size_t total_len, IpBufNode *out_node) const
+    /**
+     * Creates a memory range consisting of an initial portion
+     * of the first chunk of this memory range continued by
+     * data in a specified buffer chain.
+     * 
+     * The header_len must be less than or equal to node->len
+     * - offset and total_len must be greater than or equal to
+     * header_len.
+     * 
+     * It sets *out_node to a new IpBufNode referencing the
+     * initial portion and continuing into the given buffer
+     * chain (ptr = node->ptr, len = offset + header_len,
+     * next = cont), and returns an IpBufRef using out_node
+     * as its first buffer (node = out_node, offset = offset,
+     * tot_len = total_len).
+     * 
+     * Note that this does not "apply" the offset to the node
+     * as toNode does. This is to allow revealHeader.
+     *
+     * It is important to understand that this works by creating
+     * a new IpBufNode, because the buffer chain model cannot
+     * support this operation otherwise. The returned IpBufRef
+     * will be valid only so long as out_node remains valid.
+     */
+    inline IpBufRef subHeaderToContinuedBy (size_t header_len, IpBufNode const *cont,
+                                            size_t total_len, IpBufNode *out_node) const
     {
         AMBRO_ASSERT(node != nullptr)
         AMBRO_ASSERT(offset <= node->len)
@@ -137,76 +267,158 @@ struct IpBufRef {
         return IpBufRef{out_node, offset, total_len};
     }
     
+    /**
+     * Consume a number of bytes from the front of the memory
+     * range.
+     * 
+     * The amount must not exceed tot_len.
+     * 
+     * This moves to subsequent buffers eagerly (see processBytes).
+     */
     inline void skipBytes (size_t amount)
     {
         processBytes(amount, [](char *, size_t) {});
     }
     
+    /**
+     * Consume a number of bytes from the front of the memory
+     * memory range while copying them to the given memory
+     * location.
+     * 
+     * The amount must not exceed tot_len.
+     * 
+     * This moves to subsequent buffers eagerly (see processBytes).
+     */
     inline void takeBytes (size_t amount, char *dst)
     {
         processBytes(amount, [&](char *data, size_t len) {
-            memcpy(dst, data, len);
+            ::memcpy(dst, data, len);
             dst += len;
         });
     }
     
+    /**
+     * Consume a number of bytes from the front of the memory
+     * range while copying bytes from the given memory location
+     * into the consumed part of the range.
+     * 
+     * The amount must not exceed tot_len.
+     * 
+     * This moves to subsequent buffers eagerly (see processBytes).
+     */
     inline void giveBytes (size_t amount, char const *src)
     {
         processBytes(amount, [&](char *data, size_t len) {
-            memcpy(data, src, len);
+            ::memcpy(data, src, len);
             src += len;
         });
     }
     
-    inline void giveBuf (IpBufRef buf)
+    /**
+     * Consume a number of bytes from the front of the memory
+     * range while copying bytes from another memory range
+     * into the consumed part of the range.
+     * 
+     * The number of bytes consumed and copied is equal to
+     * the length of the other memory range (src), and must
+     * not exceed the length of this memory range.
+     * 
+     * This moves to subsequent buffers eagerly (see processBytes).
+     */
+    inline void giveBuf (IpBufRef src)
     {
-        buf.processBytes(buf.tot_len, [&](char *data, size_t len) {
-            giveBytes(len, data);
+        processBytes(src.tot_len, [&](char *data, size_t len) {
+            src.takeBytes(len, data);
         });
     }
     
-    inline size_t advanceToData ()
-    {
-        AMBRO_ASSERT(tot_len > 0)
-        
-        while (true) {
-            size_t chunk_len = getChunkLength();
-            if (AMBRO_LIKELY(chunk_len > 0)) {
-                return chunk_len;
-            }
-            bool ok = nextChunk();
-            AMBRO_ASSERT(ok)
-        }
-    }
-    
+    /**
+     * Consume and return a single byte from the front of the
+     * memory range.
+     * 
+     * The tot_len must be positive.
+     * 
+     * This moves to subsequent buffers eagerly (see processBytes).
+     */
     inline char takeByte ()
     {
         AMBRO_ASSERT(tot_len > 0)
         
-        advanceToData();
-        char ch = *getChunkPtr();
-        offset++;
-        tot_len--;
+        char ch;
+        processBytes(1, [&](char *data, size_t len) {
+            ch = *data;
+        });
         return ch;
     }
     
+    /**
+     * Consume a number of bytes from the front of the memory
+     * range while processing them with a user-specified function.
+     * 
+     * The function will be called on the subsequent contiguous
+     * chunks of the consumed part of this memory range. It will
+     * be passed two arguments: a char pointer to the start of
+     * the chunk, and a size_t length of the chunk. The function
+     * will not be called on zero-sized chunks.
+     * 
+     * This function moves forward to subsequent buffers eagerly.
+     * This means that when there are no more bytes to be
+     * processed, it will move to the next buffer as long as
+     * it is currently at the end of the current buffer and there
+     * is a next buffer.
+     * 
+     * This eager moving across buffer is useful when the buffer
+     * chain is a ring buffer, so that the offset into the buffer
+     * will remain always less than the buffer size, never becoming
+     * equal.
+     */
     template <typename Func>
     inline void processBytes (size_t amount, Func func)
     {
+        AMBRO_ASSERT(node != nullptr)
         AMBRO_ASSERT(amount <= tot_len)
         
-        while (amount > 0) {
-            size_t chunk_len = advanceToData();
-            size_t chunk_amount = APrinter::MinValue(chunk_len, amount);
+        while (true) {
+            AMBRO_ASSERT(offset <= node->len)
+            size_t rem_in_buf = node->len - offset;
             
-            func(getChunkPtr(), chunk_amount);
+            if (rem_in_buf > 0) {
+                if (amount == 0) {
+                    return;
+                }
+                
+                size_t take = APrinter::MinValue(rem_in_buf, amount);
+                func(getChunkPtr(), take);
+                
+                tot_len -= take;
+                
+                if (take < rem_in_buf || node->next == nullptr) {
+                    offset += take;
+                    AMBRO_ASSERT(amount == take)
+                    return;
+                }
+                
+                amount -= take;
+            } else {
+                if (node->next == nullptr) {
+                    AMBRO_ASSERT(amount == 0)
+                    return;
+                }
+            }
             
-            offset += chunk_amount;
-            tot_len -= chunk_amount;
-            amount -= chunk_amount;
+            node = node->next;
+            offset = 0;
         }
     }
     
+    /**
+     * Return a memory range that is an initial part of
+     * this memory range.
+     * 
+     * It returns an IpBufRef with the same node and offset
+     * but with tot_len equal to new_tot_len. The new_tot_len
+     * must not exceed tot_len.
+     */
     inline IpBufRef subTo (size_t new_tot_len) const
     {
         AMBRO_ASSERT(new_tot_len <= tot_len)
